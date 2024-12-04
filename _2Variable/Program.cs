be61b2c7873f35fe664c09aa27ed1fb286aa8fb4
stormz <21111062080@hunre.edu.vn>
2024-12-05 01:36:11 +0700

    day 1: learn variable

---
+++ _2Variable/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 123;
+a = 30;
+float b = 3.14f;
+string c = "duc";
+char d = 'a';
+bool check = true;
+decimal de = 152M;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(check);
+Console.WriteLine(de);
---
+++ _3PractiveVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 20;
+int b = 30;
+float c = 1.2f;
+float d = 1.4f;
+int sum = a+b;
+float f = c+d;
+float div = d / c;
+float mul = c * d;
+int p, o, i;
+Console.WriteLine("Sum of " + a + " + " + b + " = " + sum);
+Console.WriteLine("Sum of " + c + " + " + d + " = " + f);
+Console.WriteLine("Div of " + d + " / " + c + " = " + div);
+Console.WriteLine("Mul of " + c + " * " + d + " = " + mul);
