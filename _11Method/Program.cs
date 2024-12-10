6a4b3e77d254353f83f08b2117e1234bf1abd6e3
stormz <21111062080@hunre.edu.vn>
2024-12-10 23:01:11 +0700

    Day 5: Method

---
+++ _11Method/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _10Chapter1SummaryExercises
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Add(3,4);
+        }
+        private static void Add(int a, int b)
+        {
+            Console.WriteLine($"{a+b}");
+        }
+    }
+}
