65b0b6c637bd3e29b08470fc2d4cffbbb4483e1b
stormz <21111062080@hunre.edu.vn>
2024-12-10 22:43:45 +0700

    Day 5: Chapter 1 Exercises

---
+++ _10Chapter1SummaryExercises/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _10Chapter1SummaryExercises
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BÀI 1: TỔNG 2 SỐ NGUYÊN
+            /*
+             * Console.Write("Enter a:");
+               var a = Console.ReadLine();
+               bool parsed = int.TryParse(a, out var result);
+               
+               Console.Write("Enter b:");
+               var b = Console.ReadLine();
+               bool parsed2 = int.TryParse(b, out var result2);
+
+               if (parsed && parsed2)
+               {
+                   Console.WriteLine($"{result} + {result2} = {result+result2}");
+               }else
+               {
+                   Console.WriteLine("Invalid input.");
+               }
+             */
+            //BÀI 2: VIẾT CHƯƠNG TRÌNH NHẬP VÀO HỌ TÊN, SỐ ĐIỆN THOẠI,VÀ GIỚI TÍNH ĐỂ IN RA THÔNG TIN
+            /*
+             * Console.Write("Enter name: ");
+               string name = Console.ReadLine();
+               Console.Write("Enter age: ");
+               string age = Console.ReadLine();
+               Console.Write("Enter sex: ");
+               string sex = Console.ReadLine();
+               
+               Console.WriteLine($"{name}, {age}, {sex}");
+             */
+            
+            //BÀI 3: VIẾT CHƯƠNG TRÌNH NHẬP VÀO BÁN KÍNH HÌNH TRÒN, TÍNH DIỆN TÍCH 
+            // Console.Write("Enter r: ");
+            // var r = Console.ReadLine();
+            // const float pi = 3.1415f;
+            // bool check = float.TryParse(r, out var result);
+            // if (check)
+            // {
+            //     Console.WriteLine($"Diện tích hình tròn là {pi*(result*result)}");
+            // }
+        }   
+    }
+}
