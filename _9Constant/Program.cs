3239aaf0512418d83640af2f1fe71ed772c459ef
stormz <21111062080@hunre.edu.vn>
2024-12-10 22:46:04 +0700

    Day 5: Constant

---
+++ _9Constant/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _9Constant
+{
+    internal class Program
+    {
+        //Const không cho phép thay đổi giá trị
+        const int intValue = 15;
+        const string stringValue = "Hello World";
+        const Boolean check = true;
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(intValue);
+            Console.WriteLine(stringValue);
+            Console.WriteLine(check);
+            
+        }   
+    }
+}
